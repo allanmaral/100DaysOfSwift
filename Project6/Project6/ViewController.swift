3aeeead812abf2d9e48cc8dfd40a75d441d8381b
Allan Amaral <allanmaralr@gmail.com>
2022-08-15 20:23:44 -0300

    feat: add project 6 implementation

---
+++ Project6/Project6/ViewController.swift
@@ -0,0 +1,127 @@
+//
+//  ViewController.swift
+//  Project6
+//
+//  Created by Allan Amaral on 03/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet weak var optionsContainer: UIView!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var countries = [String]()
+    var questionsSoFar = 0
+    let numberOfQuestions = 10
+    var correctAnswer = 0 {
+        didSet {
+            countryLabel.text = countries[correctAnswer].uppercased()
+        }
+    }
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        for button in buttons {
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.lightGray.cgColor
+            button.layer.cornerRadius = 16
+            button.clipsToBounds = true
+        }
+        
+        optionsContainer.layer.cornerRadius = 32
+        optionsContainer.clipsToBounds = true
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor.systemBlue.cgColor, UIColor.systemPink.cgColor]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        askQuestion()
+        
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor).with(priority: 250)
+        ])
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        questionsSoFar += 1
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        for index in buttons.indices {
+            buttons[index].setImage(UIImage(named: countries[index]), for: .normal)
+        }
+    }
+    
+    func answerQuestion(with index: Int) {
+        var title: String
+        var message: String
+        
+        if index == correctAnswer {
+            score += 1
+            title = "Correct!"
+            message = "Your score is \(score)"
+        } else {
+            score -= 1
+            title = "Wrong!"
+            message = "That's the flag of \(countries[index].uppercased())"
+        }
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: {_ in
+            if self.questionsSoFar >= self.numberOfQuestions {
+                self.endGame()
+            } else {
+                self.askQuestion()
+            }
+        }))
+        
+        present(ac, animated: true)
+    }
+    
+    func endGame() {
+        let title = "Game Over!"
+        let message = "Your score is \(score)/\(numberOfQuestions)"
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Play again", style: .default, handler: {_ in
+            self.questionsSoFar = 0
+            self.score = 0
+            self.askQuestion()
+        }))
+        
+        present(ac, animated: true)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        answerQuestion(with: sender.tag)
+    }
+    
+}
+
+extension NSLayoutConstraint {
+    
+    func with(priority: Float) -> NSLayoutConstraint {
+        self.priority = UILayoutPriority(rawValue: priority)
+        return self
+    }
+    
+    func with(priority: UILayoutPriority) -> NSLayoutConstraint {
+        self.priority = priority
+        return self
+    }
+    
+}
